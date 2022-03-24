b54f22d6e64b524131bdae2faa808139284530ad
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-03-24 01:01:03 +0100

    running binary test file from fuzzer and sending data from binary test to black hole of linux

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -4,12 +4,14 @@
 #include <sys/stat.h>
 #include <vector>
 #include <sstream>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     int fd;
     char myfifo [] = "myfifo";
     std::vector<uint64_t> data;
+    int save_out;
 
     if(mkfifo(myfifo, 0777) == -1){
         printf("pipe sa neotvorila");
@@ -30,18 +32,51 @@ int main(int argc, char* argv[])
         default:
         {
             //parent
+            wait(NULL);
+            switch(fork()){
+                case -1:
+                {
+                    printf("fork nefunguje normalne");
+                    return -1;
+                }
+                case 0:
+                {
+                    save_out = dup(STDOUT_FILENO);
+                    if(save_out == -1)
+                    {
+                        fprintf(stderr,"Error in dup(STDOUT_FILENO)\n");
+                        exit(EXIT_FAILURE);
+                    }
 
-            uint64_t address{0};
-            fd = open(myfifo, O_RDONLY);
-            while(read(fd, &address, sizeof(uint64_t))){
-                std::cout << address << std::endl;
-                data.push_back(address);
+                    int devNull = open("/dev/null",O_WRONLY);
+                    if(devNull == -1)
+                    {
+                        fprintf(stderr,"Error in open('/dev/null',0)\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    int dup2Result = dup2(devNull, STDOUT_FILENO);
+                    if(dup2Result == -1)
+                    {
+                        fprintf(stderr,"Error in dup2(devNull, STDOUT_FILENO)\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    execl("./a.out", "./a.out", (char*) NULL);
+                }
+                default:
+                {
+                    uint64_t address{0};
+                    fd = open(myfifo, O_RDONLY);
+                    while(read(fd, &address, sizeof(uint64_t))){
+                        std::cout << address << std::endl;
+                        data.push_back(address);
+                    }
+                    close(fd);   
+                    unlink(myfifo);
+                }
             }
-
-            close(fd);   
         }
     }
-
-    unlink(myfifo);
     return 0;
 }
