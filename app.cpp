4bfa82776af83c43f89d7744fc9d79e152e75694
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-22 19:25:50 +0200

    just trying new things

--- app.cpp
+++ app.cpp
@@ -1,11 +1,15 @@
 #include "binary-file-instrumentation.hpp"
+#include "code-coverage.hpp"
 #include "iostream"
 
 int main()
 {
     BinaryFileInstrumentation binaryFileInstrumentation;
+    CodeCoverage codeCoverage;
+
     binaryFileInstrumentation.start();
-    binaryFileInstrumentation.printData();
+    codeCoverage.start();
+    codeCoverage.printData();
  
     return 0;
 }
