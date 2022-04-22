05d182763147cb782109ac131499eb32e256d23a
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-22 19:25:58 +0200

    update example

--- example.cpp
+++ example.cpp
@@ -4,10 +4,15 @@ int main(){
     int x = 3;
     int y = 4;
 
-    if((x + y) < 8){
-        std::cout << "menej ako 8";
-    } else {
-        std::cout << "viac ako 8";
+
+    for(int i = 0; i < 4; i++)
+    {
+        if((x + y) < 8)
+        {
+            std::cout << "menej ako 8";
+        } else {
+            std::cout << "viac ako 8";
+        }
     }
 
     return 0;
