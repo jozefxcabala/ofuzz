ae9c84f00b00f04018c4c5673c267fd7ba763c46
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-24 14:07:10 +0200

    implementation file name and new parm constructor for sample

--- sample.cpp
+++ sample.cpp
@@ -4,6 +4,13 @@ Sample::Sample()
 {
 }
 
+Sample::Sample(std::string data, CodeCoverage codeCoverage, std::string fileName)
+{
+    setData(data);
+    setCodeCoverage(codeCoverage);
+    setFileName(fileName);
+}
+
 void Sample::setData(std::string data)
 {
     data_ = data;
@@ -14,9 +21,19 @@ void Sample::setCodeCoverage(CodeCoverage codeCoverage)
     codeCoverage_ = codeCoverage;
 }
 
+void Sample::setFileName(std::string fileName)
+{
+    fileName_ = fileName;
+}
+
 std::string Sample::data()
 {
     return data_;
+}
+
+std::string Sample::fileName()
+{
+    return fileName_;
 }
 
 CodeCoverage Sample::codeCoverage()
--- sample.hpp
+++ sample.hpp
@@ -9,15 +9,19 @@ class Sample
 {
     CodeCoverage codeCoverage_;
     std::string data_;
+    std::string fileName_;
 
     public:
         Sample();
+        Sample(std::string data, CodeCoverage codeCoverage, std::string fileName);
 
         std::string data();
+        std::string fileName();
         CodeCoverage codeCoverage();
 
         void setData(std::string data);
         void setCodeCoverage(CodeCoverage codeCoverage);
+        void setFileName(std::string fileName);
 };
 
 #endif // SAMPLE_H
