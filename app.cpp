d4933926c602c3f9f3fa1920e9d01ee6e9136607
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-24 14:10:48 +0200

    creating fuzzer

--- app.cpp
+++ app.cpp
@@ -1,15 +1,28 @@
-#include "binary-file-instrumentation.hpp"
-#include "code-coverage.hpp"
+#include "fuzzer.hpp"
+#include "corpus-init.hpp"
 #include "iostream"
 
-int main()
+void checkParameters(int argc, char** argv)
 {
-    BinaryFileInstrumentation binaryFileInstrumentation;
-    CodeCoverage codeCoverage;
+    if (argc < 5)
+	{
+		std::cout << "Usage: ./app.out <target application> <dir for input samples> <dir for mutated files> <dir for crashes>" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
 
-    binaryFileInstrumentation.start();
-    codeCoverage.start();
-    codeCoverage.printData();
- 
+int main(int argc, char** argv)
+{
+    checkParameters(argc, argv);
+
+    BinaryFileInstrumentation binaryFileInstrumentation(argv[1]);
+    SampleProcessing sampleProcessing(argv[3]);
+    Mutation mutation;
+    CrashesProcessing crashesProcessing(argv[4]);
+    CorpusInit corpusInit(argv[2], sampleProcessing, 10);
+    std::queue<Sample> corpus = corpusInit.start();
+    Fuzzer fuzzer(binaryFileInstrumentation, sampleProcessing, mutation, crashesProcessing, corpus);
+    //fuzzer.start()
+
     return 0;
 }
