aca405976acacb5de196e58093588a1ed7842b75
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-26 18:36:04 +0200

    refactoring sample

--- sample.cpp
+++ sample.cpp
@@ -2,33 +2,16 @@
 
 Sample::Sample()
 {
+
 }
 
-Sample::Sample(std::string data, CodeCoverage codeCoverage, std::string fileName)
+Sample::Sample(SampleProcessing sampleProcessingA, CodeCoverage codeCoverageA, std::string fileNameA, CrashesProcessing crashesProcessingA, Mutation mutationA)
 {
-    setData(data);
-    setCodeCoverage(codeCoverage);
-    setFileName(fileName);
-}
-
-void Sample::setData(std::string data)
-{
-    data_ = data;
-}
-
-void Sample::setCodeCoverage(CodeCoverage codeCoverage)
-{
-    codeCoverage_ = codeCoverage;
-}
-
-void Sample::setFileName(std::string fileName)
-{
-    fileName_ = fileName;
-}
-
-std::string Sample::data()
-{
-    return data_;
+    setSampleProcessing(sampleProcessingA);
+    setCodeCoverage(codeCoverageA);
+    setFileName(fileNameA);
+    setCrashesProcessing(crashesProcessingA);
+    setMutation(mutationA);
 }
 
 std::string Sample::fileName()
@@ -39,4 +22,44 @@ std::string Sample::fileName()
 CodeCoverage Sample::codeCoverage()
 {
     return codeCoverage_;
+}
+
+SampleProcessing Sample::sampleProcessing()
+{
+    return sampleProcessing_;
+}
+
+CrashesProcessing Sample::crashesProcessing()
+{
+    return crashesProcessing_;
+}
+
+Mutation Sample::mutation()
+{
+    return mutation_;
+}
+
+void Sample::setFileName(std::string fileName)
+{
+    fileName_ = fileName;
+}
+
+void Sample::setCodeCoverage(CodeCoverage codeCoverage)
+{
+    codeCoverage_ = codeCoverage;
+}
+
+void Sample::setSampleProcessing(SampleProcessing sampleProcessing)
+{   
+    sampleProcessing_ = sampleProcessing;
+}
+
+void Sample::setCrashesProcessing(CrashesProcessing crashesProcessing)
+{
+    crashesProcessing_ = crashesProcessing;
+}
+
+void Sample::setMutation(Mutation mutation)
+{
+    mutation_ = mutation;
 }
--- sample.hpp
+++ sample.hpp
@@ -1,4 +1,7 @@
 #include "code-coverage.hpp"
+#include "crashes-processing.hpp"
+#include "mutation.hpp"
+#include "sample-processing.hpp"
 
 #include "string"
 
@@ -7,21 +10,28 @@
 
 class Sample 
 {
-    CodeCoverage codeCoverage_;
-    std::string data_;
     std::string fileName_;
+    CodeCoverage codeCoverage_;
+    SampleProcessing sampleProcessing_;
+    CrashesProcessing crashesProcessing_;
+    Mutation mutation_;
 
     public:
         Sample();
-        Sample(std::string data, CodeCoverage codeCoverage, std::string fileName);
+        Sample(SampleProcessing sampleProcessing, CodeCoverage codeCoverage, std::string fileName, CrashesProcessing crashesProcessing, Mutation mutation);
 
-        std::string data();
         std::string fileName();
         CodeCoverage codeCoverage();
+        SampleProcessing sampleProcessing();
+        CrashesProcessing crashesProcessing();
+        Mutation mutation();
 
-        void setData(std::string data);
-        void setCodeCoverage(CodeCoverage codeCoverage);
         void setFileName(std::string fileName);
+        void setCodeCoverage(CodeCoverage codeCoverage);
+        void setSampleProcessing(SampleProcessing sampleProcessing);
+        void setCrashesProcessing(CrashesProcessing crashesProcessing);
+        void setMutation(Mutation mutation);
+        
 };
 
 #endif // SAMPLE_H
