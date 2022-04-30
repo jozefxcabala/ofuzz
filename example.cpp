c56d716011bb7e478b16aca20ea74859bfbfefb1
Jozef Cabala <jozef.cabala.ml@gmail.com>
2022-04-30 14:47:55 +0200

    changing example file

--- example.cpp
+++ example.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    for(int i = 0; i < std::stoi(argv[1]); i++)
+    for(int i = 0; i < 3; i++)
     {
         if((x + y) < 8)
         {
